fa2753b176ad87597a3b9bac726d5138aadd21fd
Fernando Chavez <alvaro1221x@gmail.com>
2020-07-08 20:16:18 -0500

    changes

--- src/app/pages/authorization/auth-routing.module.ts
+++ src/app/pages/authorization/auth-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
+import { RegistryComponent } from './registry/registry.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'registry', component: RegistryComponent },
 ];
 
 @NgModule({
