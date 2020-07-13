129e3e0c1cbf1a8327cdbc3b4060bde71886cab3
katriel <katriel181920@gmail.com>
2020-07-13 14:46:58 -0500

    cliente-pedidos-v01

--- src/app/pages/index/cliente/pedidos/pedidos.component.ts
+++ src/app/pages/index/cliente/pedidos/pedidos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../../../../_service/order.service';
+import { OrderBean } from 'src/app/_model/OrderBean';
+import { throwMatDialogContentAlreadyAttachedError } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-pedidos',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PedidosComponent implements OnInit {
 
-  constructor() { }
+  ordenes:OrderBean[];
+
+  constructor(
+    private pedidos: OrderService,
+  ) { }
 
   ngOnInit(): void {
+
+
+    this.pedidos.getListOrderByUserId().subscribe(data=>{
+      this.ordenes=data;
+    })
+
   }
 
 }
