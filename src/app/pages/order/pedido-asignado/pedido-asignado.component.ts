546283d3865a04e7174730b0f21424027bf1b3f3
Jordy <jordy.sarmiento@unmsm.edu.pe>
2020-08-10 16:35:34 -0500

    zz

--- src/app/pages/order/pedido-asignado/pedido-asignado.component.ts
+++ src/app/pages/order/pedido-asignado/pedido-asignado.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/_service/user.service';
+import { MatDialog } from '@angular/material/dialog';
+import { UserBean } from 'src/app/_model/UserBean';
 
 @Component({
   selector: 'app-pedido-asignado',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PedidoAsignadoComponent implements OnInit {
 
-  constructor() { }
+  userSelect:UserBean;
+  listofUsers:UserBean[];
+
+  constructor(
+    private userService:UserService, private dialog:MatDialog
+  ) { }
 
   ngOnInit(): void {
+
+    this.userService.getDeliveryUserList().subscribe(data =>{
+      this.listofUsers = data;
+    })
+
   }
 
 }
