2558806b58657e6a31eaf91daacf9acaba743df3
felipe jimenez <new_win2100@hotmail.com>
2020-09-11 16:28:29 -0500

    boton

---
+++ src/app/pages/index/search-dish/search-dish.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-dish',
+  templateUrl: './search-dish.component.html',
+  styleUrls: ['./search-dish.component.scss']
+})
+export class SearchDishComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
