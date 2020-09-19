b683ce84150b93b18b1ae9d15fe7b2e26624e42a
= <jose.condori5@unmsm.edu.pe>
2020-09-18 19:59:41 -0500

    cambios v3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   // HOST:"http://localhost:8080",
-  HOST: "https://hidden-wildwood-21108.herokuapp.com",
-  //HOST: "https://pallevar-backend-2020.herokuapp.com",
+  // HOST: "https://hidden-wildwood-21108.herokuapp.com",
+  HOST: "https://pallevar-backend-2020.herokuapp.com",
   TOKEN_NAME : 'access_token',
   TOKEN_AUTH_USERNAME: 'pallevarR',
   TOKEN_AUTH_PASSWORD: 'pallevarx',
