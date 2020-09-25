bfa14f5a0ff6eee6506445798ba0dff2367e449d
Fernando Chavez <alvaro1221x@gmail.com>
2020-09-24 21:01:36 -0500

    changes fch

--- src/app/pages/index/search-dish/search-dish.component.ts
+++ src/app/pages/index/search-dish/search-dish.component.ts
@@ -3,19 +3,10 @@ import { ActivatedRoute } from '@angular/router';
 import { MenuDayProductService } from 'src/app/_service/menu-day-product.service';
 import { MenuDayProductBean } from 'src/app/_model/MenuDayProductBean';
 import { ProductService } from 'src/app/_service/product.service';
-<<<<<<< HEAD
-import { SharedService } from 'src/app/_service/shared.service';
-import { DomSanitizer } from '@angular/platform-browser';
-import { OrganizationService } from 'src/app/_service/organization.service';
-import { CompanyBean } from 'src/app/_model/CompanyBean';
-import { CompanyNameAndProductsDTO } from 'src/app/_DTO/CompanyNameAndProductsDTO';
-import { ProductBean } from 'src/app/_model/ProductBean';
-=======
 import { DomSanitizer } from '@angular/platform-browser';
 import { CompanyNameAndProductsDTO } from 'src/app/_DTO/CompanyNameAndProductsDTO';
 import { OrganizationService } from 'src/app/_service/organization.service';
 import { SharedService } from 'src/app/_service/shared.service';
->>>>>>> origin/buscar_prod_especifico
 
 @Component({
   selector: 'app-search-dish',
@@ -25,67 +16,13 @@ import { SharedService } from 'src/app/_service/shared.service';
 export class SearchDishComponent implements OnInit {
 
 
-<<<<<<< HEAD
-  listaPlatillos: MenuDayProductBean[] = [];
-  platilloBuscado: string;
-  listaPlatillosPorEmpresa: MenuDayProductBean[] = [];
-  empresas: CompanyBean[];
-  compañiaYproducto: CompanyNameAndProductsDTO[];
-  listaDTO: CompanyNameAndProductsDTO[] = [];   //////////////////////////////
-  xd: any;
-  dto: CompanyNameAndProductsDTO[];
-=======
 listaPlatillos: MenuDayProductBean[] = [];
 platilloBuscado:string;
 listaEmpresaConProductos: CompanyNameAndProductsDTO[];
->>>>>>> origin/buscar_prod_especifico
 
   ListaEmpresaConProductos: CompanyNameAndProductsDTO[];
 
 
-<<<<<<< HEAD
-
-  constructor(
-    private _activatedRoute: ActivatedRoute,
-    private _menuDayProductService: MenuDayProductService,
-    private productService: ProductService,
-    public sharedService: SharedService,
-    private sanitization: DomSanitizer,
-    private _organizationService: OrganizationService) { }
-
-  ngOnInit(): void {
-    //this.platilloBuscado = this._activatedRoute.snapshot.params['nameDish'];
-    //this._menuDayProductService.getSearchPlatillos(this.platilloBuscado).subscribe(data=>{
-    //this.listaPlatillos = data;
-    //})
-    this._activatedRoute.params.subscribe(params => {
-      this.platilloBuscado = params['nameDish'];
-      this.ListaEmpresaConProductos = [];
-      this._menuDayProductService.getSearchPlatillos(this.platilloBuscado).subscribe(data => {
-        console.log(Object.entries(data.dataList));
-        for (var [organizationId, value] of Object.entries(data.dataList)) {
-          console.log(organizationId);
-          this._organizationService.getCompanyById(parseInt(organizationId)).subscribe(data =>{
-            for( var [keyProduct, valueProduct] of Object.entries(value)){
-              console.log(valueProduct)
-            }
-          })
-          
-        }
-        
-     
-    })
-  })
-}
-
-
-  activatedPhoto(data: any) {
-    for (const m of data) {
-      this.productService.getPhotoById(m.product.id).subscribe(photo => {
-        const reader = new FileReader();
-        reader.readAsDataURL(photo);
-        reader.onloadend = () => {
-=======
   constructor(
     private _activatedRoute:ActivatedRoute, 
     private _menuDayProductService:MenuDayProductService, 
@@ -126,16 +63,11 @@ listaEmpresaConProductos: CompanyNameAndProductsDTO[];
         const reader = new FileReader();
         reader.readAsDataURL(photo);
         reader.onloadend = () =>{
->>>>>>> origin/buscar_prod_especifico
           const base64 = reader.result;
           m.product._foto = this.setterPhoto(base64);
           m.product._isFoto = true;
         };
-<<<<<<< HEAD
-        this.sharedService.loading = false;
-=======
         this._sharedService.loading = false;
->>>>>>> origin/buscar_prod_especifico
       });
     }
   }
