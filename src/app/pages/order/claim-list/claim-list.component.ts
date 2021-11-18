aaeaaffa155ed623b7abb82edc15c35b46a014b3
Jordy Sarmiento Espinoza <jordysarmiento@gmail.com>
2021-11-17 21:51:12 -0500

    Cambios

--- src/app/pages/order/claim-list/claim-list.component.ts
+++ src/app/pages/order/claim-list/claim-list.component.ts
@@ -47,11 +47,11 @@ export class ClaimListComponent implements OnInit {
     });
 
   }
-  public openDialog(complaint?: ComplaintBean) {
+  /* public openDialog(complaint?: ComplaintBean) {
     let complaintSelect = complaint != null ? complaint : new ComplaintBean();
     this.dialog.open(ClaimDetailComponent, {
       width: '729px', 
       data: complaintSelect
     }); 
-  }
+  } */
 }
